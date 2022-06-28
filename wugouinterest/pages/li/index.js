2785c158a86ff164938f27f24e44f4bbfef7f27b
xufang <xufang@yy.com>
2022-06-29 00:14:55 +0800

    部分厘相关逻辑

--- wugouinterest/pages/li/index.js
+++ wugouinterest/pages/li/index.js
@@ -5,10 +5,13 @@ Page({
    * 页面的初始数据
    */
   data: {
+    inputLi: '',
+    outputRes: '',
+    selectedValue: 'month',
     items: [
-      {value: 'M', name: '月息', checked: 'true'},
-      {value: 'Y', name: '年息'},
-      {value: 'D', name: '日息'}
+      {value: 'month', name: '月息', checked: 'true'},
+      {value: 'year', name: '年息'},
+      {value: 'day', name: '日息'}
     ],
     desc: "厘，中国大陆地区曾经使用的一个利率单位，根据所述为年利率、月利率或日利率，每厘分别为百分之一、千分之一或万分之一。每厘为十毫，十厘为一分。\n举例:\n年息四厘，即年利率为4%，本金1000元，每年利息为40元。\n月息三厘五毫，即月利率为3.5‰，本金1000元，每月利息为3.5元。\n日息一分二厘，即日利率为12‱，本金1000元，每日利息为1.2元。"
   },
@@ -19,6 +22,11 @@ Page({
     const items = this.data.items
     for (let i = 0, len = items.length; i < len; ++i) {
       items[i].checked = items[i].value === e.detail.value
+      if(items[i].checked == true) {
+        this.setData({
+          selectedValue : items[i].value
+        })
+      }
     }
 
     this.setData({
@@ -26,11 +34,37 @@ Page({
     })
   },
 
+  calResult(e) {
+    console.log('点击计算按钮:' + this.data.inputLi)
+    var result = 0
+    if(this.data.selectedValue == 'month' ) 
+      result = 1
+    else if(this.data.selectedValue == 'year')
+      result = 2
+    else if(this.data.selectedValue == 'day')
+      result = 3
+    this.setData({
+      outputRes: result
+    })
+  },
+
+  bindLiInput(e) {
+    this.setData({
+      inputLi: e.detail.value
+    })
+    console.log('input:' + this.data.inputLi)
+  },
+
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-
+    const items = this.data.items
+    for (let i = 0, len = items.length; i < len; ++i) {
+      items[i].value === "month"
+      items[i].checked = true
+      break
+    }
   },
 
   /**
